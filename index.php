f778e3e8552693aa4d670e0356d3ce532dd233d1
zal-byte <rizal.solehudin45@gmail.com>
2021-04-29 07:38:34 +0700

    Tenshi

---
+++ index.php
@@ -0,0 +1,276 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=0">
+	<meta name="description" content="Amatsuka Uto Our Tenshi">
+	<meta name="keyword" content="Amatsuka, uto, amatsuka uto, tenshi">
+<!-- 	<meta content='IE=edge,chrome=1' http-equiv='X-UA-Compatible'/>
+ -->	<link rel="stylesheet" type="text/css" href="style/bootstrap4.3.1.css">
+	<link rel="stylesheet" type="text/css" href="style/jquery-ui.css">
+	<link rel="stylesheet" type="text/css" href="style/font-awesome.min.css">
+	<title>Amatsuka Uto</title>
+	<script type="text/javascript" src="js/jquery.min.js"></script>
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="js/popper.min.js"></script>
+	<script type="text/javascript" src="js/kit.js"></script>
+	<style type="text/css">
+		body{
+			margin-bottom: 0;
+			margin-top: 0;
+			margin-left: 0;
+			margin-right: 0;
+			padding-bottom: 200px;
+/*			background-image:url("amatsuka_tenshi.jpg");
+			background-repeat:no-repeat;
+			background-size:cover;*/
+			font-family: googlesans;
+		}
+		.utobg{
+			background-image:url("png/gg_men.png");
+			background-position: right;
+			background-size: contain;
+			background-repeat: no-repeat;
+		}
+		@font-face{
+			font-family: googlesans;
+			src:url("font/google_sans.ttf");
+		}
+		footer{
+			padding:0;
+			position: fixed;
+			bottom:0;
+			width: 100%;
+		}
+		.nyet{
+			padding: 2px;
+		}
+		.nyet:hover{
+			background-color:lightblue;
+			padding: 3px;
+		}
+		.nyet a:hover{
+			color:darkblue;
+			padding: 2px;
+		}
+		.nabrandtext{
+			text-shadow: 2px 2px 2px black;
+		}
+		.fa-3{
+			font-size:3em!important;
+		}
+		.fa-2{
+			font-size: 2em!important;
+		}
+		.utoimg1{
+			background-image:url("png/amatsuka1.png");
+			background-repeat: no-repeat;
+			background-position: center;
+			background-size: contain;
+		}
+		.utoimg2{
+			background-image: url("png/chibi_uto.png");
+			background-repeat: no-repeat;
+			background-position: left,bottom;
+			background-size: contain;
+		}
+		.par{
+			background-color:#57BFFF;
+		}
+		@media (min-width:320px) or (min-width: 480){
+			body{
+				background-image:url("__amatsuka_uto_and_duo_indie_virtual_youtuber_and_1_more_drawn_by_yuno_p2eocene__936e02905ddbb856ab6856cc3a2b20fe.jpg");
+				background-repeat:no-repeat;
+				background-size:cover;
+				background-position: bottom;
+				background-attachment: fixed;
+			}
+			.utobg{
+				background-image:url("png/gg_men.png");
+				background-position: right;
+				background-size: contain;
+				background-repeat: no-repeat;
+			}
+		}
+	</style>
+</head>
+<body>
+	<img src="png/gg_men.png" hidden="true">
+	<header>
+		<nav class="navbar navbar-expand-lg navbar-light shadow">
+			<a class="navbar-brand nabrandtext text-white font-weight-bold" href="">Amatsuka Uto</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle Navigation"><span class="navbar-toggler-icon"></span></button>
+			<div class="collapse navbar-collapse" id="navbarNav">
+				<ul class="navbar-nav mr-auto">
+					<li class="nav-item nyet">
+						<a href="https://safebooru.org/index.php?page=post&s=list&tags=amatsuka_uto" target="_blank" style="text-decoration: none;">Amatsuka Uto | Safebooru ( source image )</a>
+					</li>
+					<li class="nav-item nyet">
+						<a href="https://virtualyoutuber.fandom.com/wiki/Amatsuka_Uto" target="_blank" style="text-decoration: none;">Amatsuka Uto | Fandom</a>
+					</li>
+				</ul>
+			</div>
+		</nav>
+	</header>	
+	<div class="container shadow">
+		<div class="carousel slide" id="demo" data-interval="false">
+			<ul class="carousel-indicators">
+				<li class="active" data-target="#demo" data-slide-to="0">
+				<li data-target="#demo" data-slide-to="1"></li>
+				<li data-target="#demo" data-slide-to="2"></li>
+			</ul>
+
+			<div class="carousel-inner">
+				<div class="carousel-item active">
+					<iframe title="Amatsuka Uto" type="text/html" height="390" style="width: 100%;" src="https://www.youtube.com/embed/UYdFBVN5CbM" frameborder="0" allowFullScreen></iframe>
+				</div>
+				<div class="carousel-item">
+					<iframe type="text/html" height="390" style="width: 100%;" src="https://www.youtube.com/embed/PvTLiKMxPXY" frameborder="0" allowFullScreen></iframe>
+				</div>
+				<div class="carousel-item">
+					<img src="amatsuka_tenshi.jpg" style="width: 100%;" height="390">
+				</div>
+			</div>
+					<a class="carousel-control-prev" href="#demo" data-slide="prev">
+				<span class="carousel-control-prev-icon"></span>
+			</a>
+			<a class="carousel-control-next" href="#demo" data-slide="next">
+				<span class="carousel-control-next-icon"></span>
+			</a>
+		</div>
+	</div>
+	<div class="container">
+		<div class="card border-0 shadow">
+			<div class="card-header bg-light utobg">
+				<h3> Amatsuka Uto </h3>
+			</div>
+			<div class="card-body pl-0 pr-0">
+				<div class="card shadow">
+					<div class="card-body">
+						<iframe src="https://www.youtube.com/embed/qRzbpSaOsOA" title="Amatsuka Uto Introduction" style="width: 100%;" height="390"></iframe>
+					</div>
+				</div>
+				<p>
+					<center>Amatsuka Uto (天使うと) is an independently produced Japanese VTuber who debuted on 14 December 2020. She streams on YouTube.</center> 
+				</p>
+				<hr>
+				<div class="card border-0 shadow">
+					<div class="card-body">
+						<p class="font-weight-bold p-2 bg-primary text-white rounded" style="text-align: center;">Basic Details</p>
+						<div class="row">
+							<div class="col-md">
+								<img src="png/chibi_uto.png" class="img-responsive" style="width: 100%;" height="360">
+							</div>
+							<div class="col-md">
+								<div class="card border-0 shadow" style="background-color: lightblue;">
+									<div class="card-body">
+										<div class="row">
+											<div class="col-sm">
+												<p class="font-weight-bold p-2 bg-primary text-white rounded">Original Name</p>
+											</div>
+											<div class="col-sm">
+												<b class="p-2 font-weight-bold par rounded text-white shadow">天使うと</b>
+											</div>	
+										</div>
+										<div class="row">
+											<div class="col-sm">
+												<p class="font-weight-bold p-2 bg-primary text-white rounded">Nickname(s)</p>
+											</div>
+											<div class="col-sm">
+												<ul class="p-2 font-weight-bold par rounded text-white shadow" type="none">
+													<li> Tenshi </li>
+													<li> Blue Pumpkin </li>
+													<li> Aoi Kabocha </li>
+												</ul>
+											</div>
+										</div>
+										<div class="row">
+											<div class="col-sm">
+												<p class="font-weight-bold p-2 bg-primary text-white rounded">Debut Date</p>
+											</div>
+											<div class="col-sm">
+												<b class="p-2 font-weight-bold par rounded text-white shadow">2020/2014</b>
+											</div>
+										</div>
+										<div class="row">
+											<div class="col-sm">
+												<p class="font-weight-bold p-2 bg-primary text-white rounded">Character Designer</p>
+											</div>
+											<div class="col-sm">
+												<ul class="p-2 font-weight-bold par rounded text-white shadow" type="none">
+													<li> Ilustrator: Nabi </li>
+													<li> Modelling: Takeshita Fungi</li>
+												</ul>
+											</div>
+										</div>
+										<div class="row">
+											<div class="col-sm">
+												<p class="font-weight-bold p-2 bg-primary text-white rounded">Affiliation</p>
+											</div>
+											<div class="col-sm">
+												<b class="p-2 font-weight-bold par rounded text-white shadow">Indenpendent</b>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<p class="font-weight-bold p-2 bg-primary text-white rounded mt-2" style="text-align: center;">Media</p>
+						<div class="row">
+							<div class="col-sm">
+								<div class="card border-0 shadow" style="background-color: lightblue;">
+									<div class="card-body">
+										<div class="row">
+											<div class="col-sm">
+												<p class="font-weight-bold p-2 bg-primary text-white rounded">Channel</p>
+											</div>
+											<div class="col-sm">
+												<a href="https://www.youtube.com/channel/UCdYR5Oyz8Q4g0ZmB4PkTD7g" target="_blank" class="btn link-btn shadow font-weight-bold p-2 text-white rounded par">天使うと / Amatsuka Uto</a>
+											</div>
+										</div>
+										<div class="row">
+											<div class="col-sm">
+												<p class="font-weight-bold p-2 bg-primary text-white rounded mt-2" style="text-align: center;">Social Media</p>
+											</div>
+										</div>
+										<div class="row">
+											<div class="col-sm" style="text-align: center;">
+												<a href="https://twitter.com/amatsukauto" target="_blank"><span class="fa fa-3 fa-twitter-square"></span></a>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="col-sm">
+								<img src="png/amatsuka1.png" class="img-responsive" height="365" style="width: 100%;">
+							</div>
+						</div>
+					</div>
+				</div>		
+				<hr>
+				<div class="container-fluid">
+					<center>
+						<p class="font-weight-bold"> Sources </p>
+						<div class="row">
+							<div class="col">
+								<a href="https://safebooru.org/index.php?page=post&s=list&tags=amatsuka_uto"alt="Image" target="_blank"><span class="fa fa-3 fa-picture-o"></span></a>
+							</div>
+							<div class="col">
+								<a href="https://virtualyoutuber.fandom.com/wiki/Amatsuka_Uto" alt="Information" target="_blank"><span class="fa fa-3 fa-file-text"></span></a>
+							</div>	
+						</div>
+					</center>
+				</div>
+			</div>
+		</div>
+	</div>
+	<footer class="container-fluid bg-light">
+		<center>
+			<p> Coded by <span style="color:skyblue;">Zappan</span> Made With <span class="fa fa-heart" style="color:red;"></span></p>
+		</center>
+	</footer>
+</body>
+</html>
+<!-- <script>
+    document.write("<iframe title='YouTube video player' type=\"text/html\" width='640' height='390' src='http://www.youtube.com/embed/UYdFBVN5CbM'frameborder='0' allowFullScreen></iframe>");
+</script> -->
